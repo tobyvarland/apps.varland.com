c99cf35a68f5820bbc8a8180081b3c91a6ec88a0
Toby Varland <toby.varland@varland.com>
2021-04-27 10:56:48 -0400

    Add links to homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,11 +28,23 @@ module ApplicationHelper
   def flash_message(msg, type)
     message = content_tag(:div, msg.html_safe, class: "message")
     icon = case type
-           when :alert then content_tag(:div, fa("exclamation", text_class: "text-danger"), class: ["icon", "rounded-start"])
-           else content_tag(:div, fa("info", text_class: "text-info"), class: ["icon", "rounded-start"])
+           when :alert then content_tag(:div, fa("exclamation"), class: ["icon", "flash-alert"])
+           else content_tag(:div, fa("info"), class: ["icon", "flash-notice"])
            end
     inner = content_tag(:div, icon + message, class: ["flash-message", "bg-light", "rounded", "shadow-sm"])
     return content_tag(:div, inner, class: ["col-12", "px-3"])
+  end
+
+  def home_page_link(icon, text, url, options = {})
+    tooltip = options.fetch(:tooltip, nil)
+    link_text = content_tag(:div, text.html_safe, class: "text")
+    icon = content_tag(:div, fa(icon), class: "icon")
+    if tooltip.blank?
+      link = link_to(icon + link_text, url, class: "home-page-link", target: "_blank")
+    else
+      link = link_to(icon + link_text, url, class: "home-page-link", target: "_blank", title: tooltip, data: {"bs-toggle": "tooltip", "bs-placement": "top", "bs-html": "true"})
+    end
+    return content_tag(:div, link, class: ["col-12", "col-md-3"])
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,10 @@ ActiveStorage.start()
 import * as bootstrap from 'bootstrap'
 import "../stylesheets/application"
 import "@fortawesome/fontawesome-free/js/all"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
