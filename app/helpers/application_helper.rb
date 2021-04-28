6ff825caef888f78039e57d9275ddab4598b4512
Toby Varland <toby.varland@varland.com>
2021-04-28 13:37:45 -0400

    Add offcanvas app navigation option

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,11 +18,12 @@ module ApplicationHelper
     badge = options.fetch(:badge, nil)
     badge_class = options.fetch(:badge_class, "bg-primary")
     badge_text_class = options.fetch(:badge_text_class, "text-white")
+    link_class = options.fetch(:link_class, [])
     icon = fa(icon, type: type, text_class: text_class)
     text = content_tag(:span, text, class: ["label", text_class])
     badge = badge.nil? ? "" : content_tag(:div, badge, class: ["badge", "rounded-pill", badge_class, badge_text_class])
     html = content_tag(:div, icon + text + badge, class: ["d-flex", "flex-row", "align-items-center", "justify-content-start"])
-    link_to_unless_current(html, url, class: "nav-link") do content_tag(:div, html, class: "navbar-text px-md-2") end
+    link_to_unless_current(html, url, class: ["nav-link"] + link_class) do content_tag(:div, html, class: "navbar-text px-md-2") end
   end
 
   def flash_message(msg, type)
