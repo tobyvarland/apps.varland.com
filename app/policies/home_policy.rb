88fd976e1b489578bcb7b95ddbdadcabdaa75f5c
Toby Varland <toby.varland@varland.com>
2021-04-29 13:56:23 -0400

    Fix employee notes permission on home page policy

--- app/policies/home_policy.rb
+++ app/policies/home_policy.rb
@@ -1,7 +1,7 @@
 class HomePolicy < Struct.new(:user, :home)
 
   def employee_notes?
-    return super || (user && user.employee_number >= 600)
+    return user && (user.permission.super_admin || user.employee_number >= 600)
   end
 
 end
