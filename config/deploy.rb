bf229bc01acf714c29686e2d3cf5120df106accd
Toby Varland <toby.varland@varland.com>
2021-05-06 11:04:29 -0400

    Don't attach photos to reject tag emails

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,6 @@ set :repo_url, "git@github.com:tobyvarland/apps.varland.com.git"
 
 set :deploy_to, "/home/varland/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/pdfs', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 append :linked_files, 'config/master.key'
 set :keep_releases, 5
