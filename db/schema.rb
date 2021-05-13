ee9cca034b611b7599c8a64ad22cb5ed418249f1
Toby Varland <toby.varland@varland.com>
2021-05-13 10:49:02 -0400

    Add active flag for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_204927) do
+ActiveRecord::Schema.define(version: 2021_05_13_144828) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -190,6 +190,7 @@ ActiveRecord::Schema.define(version: 2021_05_12_204927) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username", null: false
+    t.boolean "is_active", default: true, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
