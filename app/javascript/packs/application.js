bf987fb0a173537fceee19c36b97b532cd9ff56e
Toby Varland <toby.varland@varland.com>
2021-05-19 16:49:54 -0400

    Change javascript page load event

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,7 +31,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 });
 
-document.addEventListener("DOMContentLoaded", function(event) {
+// document.addEventListener("DOMContentLoaded", function(event) {
+$(document).on('turbolinks:load', function() {
   
   // Auto refresh if necessary.
   var $body = $("body");
