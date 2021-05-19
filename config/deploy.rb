94f7c36feea5d648913192d883d6dd7e94b93d58
Toby Varland <toby.varland@varland.com>
2021-05-19 17:22:22 -0400

    Attempt auto restart sidekiq after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,9 @@ set :deploy_to, "/home/varland/#{fetch :application}"
 append :linked_dirs, 'storage', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/pdfs', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 append :linked_files, 'config/master.key'
 set :keep_releases, 5
+
+task :restart_sidekiq do
+  on roles(:app) do
+    execute "sudo systemctl sidekiq-apps restart"
+  end
+end
