3567508351778371d22d32df8688c01433222f93
Toby Varland <toby.varland@varland.com>
2021-05-19 12:31:58 -0400

    Add part search query

--- app/controllers/vcms/production_controller.rb
+++ app/controllers/vcms/production_controller.rb
@@ -7,4 +7,37 @@ class VCMS::ProductionController < ApplicationController
     @jobs = load_json "http://vcmsapi.varland.com/jobs_on_receipt"
   end
 
+  def part_search
+    query_fields = {}
+    if params[:filters].present?
+      [:customer, :process, :part, :sub, :base_metal, :industry, :sub_industry, :on_or_after, :on_or_before, :search].each do |sym|
+        if params[:filters][sym].present?
+          query_fields[sym] = CGI.escape(params[:filters][sym])
+        end
+      end
+      if params[:filters][:active].present?
+        query_fields[:active] = (params[:filters][:active] == "Active Only" ? "true" : "false")
+      end
+      if params[:filters][:developmental].present?
+        query_fields[:developmental] = (params[:filters][:developmental] == "Developmental Only" ? "true" : "false")
+      end
+      [:valid_price, :valid_procedure].each do |sym|
+        if params[:filters][sym].present?
+          query_fields[sym] = (params[:filters][sym] == "Valid Only" ? "true" : "false")
+        end
+      end
+    end
+    url = nil
+    if query_fields.size == 0
+      url = "http://vcmsapi.varland.com/default_part_search_filters"
+    else
+      url = "http://vcmsapi.varland.com/part_search?"
+      query_fields.each {|key, value|
+        url << (url[-1] == "?" ? "" : "&") << "#{key}=#{value}"
+      }
+    end
+    # puts "🔴 #{url}"
+    @data = load_json url
+  end
+
 end
--- app/policies/vcms_policy.rb
+++ app/policies/vcms_policy.rb
@@ -1,5 +1,9 @@
 class VCMSPolicy < Struct.new(:user, :vcms)
 
+  def part_search?
+    return !user.blank?
+  end
+
   def recent_customers?
     return user && (user.permission.super_admin || user.employee_number >= 700)
   end
