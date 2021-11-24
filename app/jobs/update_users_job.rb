a85b399fdd2901c50d0c470d72c73f15af3e48f1
Toby Varland <toby.varland@varland.com>
2021-11-24 10:25:50 -0500

    Test background user updating

--- app/jobs/update_users_job.rb
+++ app/jobs/update_users_job.rb
@@ -3,7 +3,9 @@ class UpdateUsersJob < ApplicationJob
   queue_as :default
 
   def perform
-    puts "Need to update users from System i..."
+    User.where(employee_number: 937).each do |user|
+      user.update_fields_from_system_i
+    end
   end
 
 end
