744d61bf40aa60a264fa0554f545594a4c07dbc2
Toby Varland <toby.varland@varland.com>
2021-12-08 15:10:02 -0500

    Update priority validation

--- app/models/projects/item.rb
+++ app/models/projects/item.rb
@@ -4,8 +4,8 @@ class Projects::Item < ApplicationRecord
   belongs_to  :category,
               class_name: "Projects::Category",
               foreign_key: "category_id",
-              inverse_of: :items 
-              
+              inverse_of: :items
+
   # Validations.
   validates   :current_status,
               presence: true,
@@ -13,10 +13,10 @@ class Projects::Item < ApplicationRecord
   validates   :current_status_at,
               presence: true
   validates   :percent_complete,
-              numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, 
-              allow_blank: true     
+              numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 },
+              allow_blank: true
   validates   :projected_hours,
-              numericality: { greater_than: 0 }, 
+              numericality: { greater_than: 0 },
               allow_blank: true
   validate    :require_valid_priority
 
@@ -25,22 +25,19 @@ class Projects::Item < ApplicationRecord
 
   # Instance Methods.
 
-  # Sets item number to next available for category. 
+  # Sets item number to next available for category.
   def set_item_number
-    ActiveRecord::Base.transaction do 
+    ActiveRecord::Base.transaction do
       self.number = self.category.last_number_used + 1
       self.save
       self.category.last_number_used = self.number
       self.category.save
     end
   end
-  
-  # Validates priority against category. 
+
+  # Validates priority against category.
   def require_valid_priority
-    if self.category.blank? || !self.category.use_priorities
-      self.errors.add(:current_priority_at, "must be blank") unless self.current_priority_at.blank?
-      self.errors.add(:current_priority, "must be blank") unless self.current_priority.blank?
-    else
+    if self.category.present? || self.category.use_priorities
       self.errors.add(:current_priority_at, "can't be blank") if self.current_priority_at.blank?
       self.errors.add(:current_priority, "is invalid") unless [1, 2, 3].include?(self.current_priority)
     end
