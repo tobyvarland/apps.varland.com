ae1658d8e1590c05b81f5c95314e51d1428253ff
Toby Varland <toby.varland@varland.com>
2022-02-21 11:35:37 -0500

    Add debug statements to print job

--- app/jobs/quality/print_reject_tag_job.rb
+++ app/jobs/quality/print_reject_tag_job.rb
@@ -4,28 +4,36 @@ class Quality::PrintRejectTagJob < ApplicationJob
 
   def perform(reject_tag, first_page_printer, other_pages_printer)
 
+    puts "🔴 Printing reject tag..."
+
     # Generate PDF file.
     path = Rails.root.join('tmp', 'pdfs', "#{reject_tag.description}.pdf")
     pdf = Quality::RejectTagPdf.new(reject_tag)
     pdf_file = File.open path, "wb"
     pdf_file.write pdf.render
     pdf_file.close
+    puts "🔴 Generated reject tag PDF (#{path})"
 
     # Call command to print file.
     first_page_command = "lpr -P #{first_page_printer} -o page-ranges=1 #{path}"
     system(first_page_command)
+    puts "🔴 Printed first page (#{first_page_command})"
     if reject_tag.attachments.length > 0
       1.upto(reject_tag.attachments.length) do |i|
         attachments_command = "lpr -P #{other_pages_printer} -o page-ranges=#{i + 1} #{path}"
         system(attachments_command)
+        puts "🔴 Printed additional page (#{attachments_command})"
       end
     end
 
     # Delete PDF file.
     File.delete(path)
+    puts "🔴 Deleted reject tag PDF"
 
     # Print partial tag if flag set.
     if reject_tag.print_partial_tag
+
+      puts "🔴 Need to print partial order tag"
 
       # Generate PDF.
       path = Rails.root.join('tmp', 'pdfs', "#{reject_tag.description}-Partial.pdf")
@@ -33,10 +41,12 @@ class Quality::PrintRejectTagJob < ApplicationJob
       pdf_file = File.open path, "wb"
       pdf_file.write pdf.render
       pdf_file.close
+      puts "🔴 Generated partial order tag PDF (#{path})"
 
       # Print file.
       partial_tag_command = "lpr -P #{other_pages_printer} #{path}"
       system(partial_tag_command)
+      puts "🔴 Printed partial order tag (#{partial_tag_command})"
 
       # Delete PDF file.
       #File.delete(path)
