5c8b6e0506818de42142f72fb7192bef5841eff9
Toby Varland <toby.varland@varland.com>
2022-02-21 12:02:52 -0500

    Fix reject tag printing job

--- app/jobs/quality/print_reject_tag_job.rb
+++ app/jobs/quality/print_reject_tag_job.rb
@@ -4,7 +4,7 @@ class Quality::PrintRejectTagJob < ApplicationJob
 
   def perform(reject_tag, first_page_printer, other_pages_printer)
 
-    puts "🔴 Printing reject tag..."
+    #puts "🔴 Printing reject tag..."
 
     # Generate PDF file.
     path = Rails.root.join('tmp', 'pdfs', "#{reject_tag.description}.pdf")
@@ -12,28 +12,28 @@ class Quality::PrintRejectTagJob < ApplicationJob
     pdf_file = File.open path, "wb"
     pdf_file.write pdf.render
     pdf_file.close
-    puts "🔴 Generated reject tag PDF (#{path})"
+    #puts "🔴 Generated reject tag PDF (#{path})"
 
     # Call command to print file.
     first_page_command = "lpr -P #{first_page_printer} -o page-ranges=1 #{path}"
     system(first_page_command)
-    puts "🔴 Printed first page (#{first_page_command})"
+    #puts "🔴 Printed first page (#{first_page_command})"
     if reject_tag.attachments.length > 0
       1.upto(reject_tag.attachments.length) do |i|
         attachments_command = "lpr -P #{other_pages_printer} -o page-ranges=#{i + 1} #{path}"
         system(attachments_command)
-        puts "🔴 Printed additional page (#{attachments_command})"
+        #puts "🔴 Printed additional page (#{attachments_command})"
       end
     end
 
     # Delete PDF file.
     File.delete(path)
-    puts "🔴 Deleted reject tag PDF"
+    #puts "🔴 Deleted reject tag PDF"
 
     # Print partial tag if flag set.
     if reject_tag.print_partial_tag
 
-      puts "🔴 Need to print partial order tag"
+      #puts "🔴 Need to print partial order tag"
 
       # Generate PDF.
       path = Rails.root.join('tmp', 'pdfs', "#{reject_tag.description}-Partial.pdf")
@@ -41,15 +41,15 @@ class Quality::PrintRejectTagJob < ApplicationJob
       pdf_file = File.open path, "wb"
       pdf_file.write pdf.render
       pdf_file.close
-      puts "🔴 Generated partial order tag PDF (#{path})"
+      #puts "🔴 Generated partial order tag PDF (#{path})"
 
       # Print file.
       partial_tag_command = "lpr -P #{other_pages_printer} #{path}"
       system(partial_tag_command)
-      puts "🔴 Printed partial order tag (#{partial_tag_command})"
+      #puts "🔴 Printed partial order tag (#{partial_tag_command})"
 
       # Delete PDF file.
-      #File.delete(path)
+      File.delete(path)
 
       # Clear flag.
       #reject_tag.print_partial_tag = false
