747a7ca3542527a3204911f0d937cc3f4c887d34
Toby Varland <toby.varland@varland.com>
2022-02-22 09:00:54 -0500

    Add log configuration for salt spray temperature warning

---
+++ app/models/groov/salt_spray_temperature_warning.rb
@@ -0,0 +1,19 @@
+class Groov::SaltSprayTemperatureWarning < Groov::Log
+
+  # Instance methods.
+
+  # Notification settings.
+  def notification_settings
+    return {
+      enabled: true,
+      subject: "#{self.controller_name}: Salt Spray Temperature Out of Limits",
+      recipients: [FOREMAN_EMAIL, GREG_TURNER_EMAIL]
+    }
+  end
+
+  # Log details.
+  def details
+		return "<p>Salt spray temperature is out of limits. Reading: <code>#{self.reading}º F</code>. Low limit: <code>#{self.low_limit}º F</code>. High limit: <code>#{self.high_limit}º F</code>.</p>"
+  end
+
+end
