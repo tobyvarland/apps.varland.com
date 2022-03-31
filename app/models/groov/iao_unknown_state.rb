e44a28094cc4c9866a3d0961281b87879541e552
Varland Plating IT <92859975+VarlandPlatingIT@users.noreply.github.com>
2022-03-31 15:37:54 -0400

    Log format

--- app/models/groov/iao_unknown_state.rb
+++ app/models/groov/iao_unknown_state.rb
@@ -14,7 +14,12 @@ class Groov::IAOUnknownState < Groov::Log
   # Log details.
   def details
     heat_output = self.groov_data[:oven_heat].to_i == 1 ? "On" : "Off"
-		return "<p>IAO is in unknown state.</p><p><small>IAO:</small> <code>#{self.device}</code><br><small>Heat Output from Opto:</small> <code>#{heat_output}</code></p>"
+    external_solenoid = self.groov_data[:nitrogen_shutoff].to_i == 1 ? "Open" : "Closed"
+    high_flow = self.groov_data[:high_flow].to_i == 1 ? "Open" : "Closed"
+    low_flow = self.groov_data[:low_flow].to_i == 1 ? "Open" : "Closed"
+    gas_flow = self.groov_data[:gas_needed].to_i == 1 ? "Yes" : "No"
+    profile_running = self.groov_data[:profile_running].to_i == 1 ? "Yes" : "No"
+		return "<p>IAO is in unknown state.</p><p><small>IAO:</small> <code>#{self.device}</code><br><small>Heat Output from Opto:</small> <code>#{heat_output}</code><br><small>External Solenoid:</small> <code>#{external_solenoid}</code><br><small>High Flow Nitrogen Solenoid:</small> <code>#{high_flow}</code><br><small>Low Flow Nitrogen Solenoid:</small> <code>#{low_flow}</code><br><small>Gas Needed:</small> <code>#{gas_flow}</code><br><small>Profile Running:</small> <code>#{profile_running}</code><br><small>Profile Name:</small> <code>#{self.groov_data[:profile]}</code><br><small>Step #:</small> <code>#{self.groov_data[:step].to_i}</code><br><small>Step Type:</small> <code>#{self.groov_data[:step_type]}</code></p>"
   end
 
   # Returns human readable log type.
