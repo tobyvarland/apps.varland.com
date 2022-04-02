c84825d5741b4340d7d00336022015ebd371cd2f
Toby Varland <toby.varland@varland.com>
2022-04-02 12:09:35 -0400

    Bug fix

--- app/models/groov/iao_nitrogen_pack_replaced.rb
+++ app/models/groov/iao_nitrogen_pack_replaced.rb
@@ -6,7 +6,7 @@ class Groov::IAONitrogenPackReplaced < Groov::Log
   def details
     return self.format_log_data("Nitrogen pack replaced.", {
       pack: self.device,
-      replaced_by: self.groov_user
+      replaced_by: self.groov_user,
       psi_before: "#{self.groov_data[:before].to_i} PSI",
       psi_after: "#{self.groov_data[:after].to_i} PSI"
     })
