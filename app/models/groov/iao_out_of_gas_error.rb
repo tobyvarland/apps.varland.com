b490b525bc3eed90ae2682b431fabd16c3925de3
Toby Varland <toby.varland@varland.com>
2022-04-02 17:27:20 -0400

    Fix log format

--- app/models/groov/iao_out_of_gas_error.rb
+++ app/models/groov/iao_out_of_gas_error.rb
@@ -5,10 +5,10 @@ class Groov::IAOOutOfGasError < Groov::Log
   # Log details.
   def details
     return self.format_log_data("IAO system is out of nitrogen gas and relying on emergency supply.", {
-      low_limit: "#{self.limit.to_f.round(3)} PSI",
-      pack_1: "#{self.groov_data[:pack_1].to_f.round(3)} PSI",
-      pack_2: "#{self.groov_data[:pack_2].to_f.round(3)} PSI",
-      emergency_supply: "#{self.groov_data[:emergency_pack].to_f.round(3)} PSI"
+      low_limit: "#{self.limit.to_i} PSI",
+      pack_1: "#{self.groov_data[:pack_1].to_i} PSI",
+      pack_2: "#{self.groov_data[:pack_2].to_i} PSI",
+      emergency_supply: "#{self.groov_data[:emergency_pack].to_i} PSI"
     })
   end
 
