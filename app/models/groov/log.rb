52f1d8f3cc911393d297ed43b8a0c5315db85659
Toby Varland <toby.varland@varland.com>
2022-04-06 11:13:19 -0400

    Update email recipients for Groov logs

--- app/models/groov/log.rb
+++ app/models/groov/log.rb
@@ -58,7 +58,7 @@ class Groov::Log < ApplicationRecord
     recipients = []
     recipients << GROOV_IT_EMAIL if flags[0]
     recipients << GROOV_MAINTENANCE_EMAIL if flags[1]
-    recipients << TOBY_VARLAND_EMAIL if flags[2]
+    recipients << FOREMAN_EMAIL if flags[2]
     recipients << GROOV_PRODUCTION_EMAIL if flags[3]
     recipients << GROOV_LAB_EMAIL if flags[4]
     recipients << GROOV_QC_EMAIL if flags[5]
