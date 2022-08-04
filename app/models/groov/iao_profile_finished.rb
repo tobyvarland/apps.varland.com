a1fb4bdb86b802671208e6d86869d1346f73e874
Toby Varland <toby.varland@varland.com>
2022-08-04 08:43:31 -0400

    Update details for IAO profile finished message

--- app/models/groov/iao_profile_finished.rb
+++ app/models/groov/iao_profile_finished.rb
@@ -12,13 +12,15 @@ class Groov::IAOProfileFinished < Groov::Log
       total_soak_time: self.humanize_seconds(self.groov_data[:soak_time].to_i),
       total_cooldown_time: self.humanize_seconds(self.groov_data[:cooldown_time].to_i),
       total_time_with_gas_flow: self.humanize_seconds(self.groov_data[:gas_time].to_i),
+      total_profile_time: self.humanize_seconds(self.groov_data[:profile_time].to_i),
       profile_started_at: self.groov_data[:profile_started],
       purge_ended_at: self.groov_data[:purge_ended],
       soak_started_at: self.groov_data[:soak_started],
       soak_ended_at: self.groov_data[:soak_ended],
       gas_off_at: self.groov_data[:gas_off],
       profile_ended_at: self.groov_data[:profile_ended],
-      switched_to_cooling_profile: (self.groov_data[:used_cooling_profile].to_i == 1 ? "Yes" : "No")
+      switched_to_cooling_profile: (self.groov_data[:used_cooling_profile].to_i == 1 ? "Yes" : "No"),
+      total_gas_usage: self.groov_data[:total_gas_usage]
     })
   end
 
