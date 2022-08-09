625400b0e892618c63d6285bcd72176569fe5637
Toby Varland <toby.varland@varland.com>
2022-08-09 10:56:12 -0400

    Add new Groov log definition

---
+++ app/models/groov/sludge_dryer_gas_warning.rb
@@ -0,0 +1,10 @@
+class Groov::SludgeDryerGasWarning < Groov::Log
+
+  # Instance methods.
+
+  # Log details.
+  def details
+    return "Power to the sludge dryer is turned on but gas has been turned off for too long."
+  end
+
+end
