288f9f53caaf2966f7ec889ea57c468f5790b3f2
straderm <38769197+straderm@users.noreply.github.com>
2022-08-11 14:31:20 -0400

    Add new logs

---
+++ app/models/groov/ph_recorder_button_too_long.rb
@@ -0,0 +1,13 @@
+class Groov::pHRecorderButtonTooLong < Groov::Log
+
+  # Instance methods.
+
+  # Log details.
+  def details
+    return self.format_log_data("pH recorder button held too long.", {
+      reading: self.humanize_seconds(self.reading, 3),
+      limit: self.humanize_seconds(self.reading, 3)
+    })
+  end
+
+end
---
+++ app/models/groov/ph_recorder_button_water_flow.rb
@@ -0,0 +1,10 @@
+class Groov::pHRecorderButtonWaterFlow < Groov::Log
+
+  # Instance methods.
+
+  # Log details.
+  def details
+    return self.format_log_data("pH recorder button held with water flowing out of the clarifier.")
+  end
+
+end
