5bc50b01594e7a7b578726d7d5bbadd344e70d57
Toby Varland <toby.varland@varland.com>
2022-08-26 15:26:51 -0400

    New Groov log type

---
+++ app/models/groov/raven_orp_warning.rb
@@ -0,0 +1,15 @@
+class Groov::RavenOrpWarning < Groov::Log
+
+  # Instance methods.
+
+  # Log details.
+  def details
+    return self.format_log_data("Raven ORP reading out of limits.", {
+      raven: self.device,
+      reading: "#{self.reading.to_f.round(3)}",
+      low_limit: "#{self.low_limit.to_f.round(3)}",
+      high_limit: "#{self.high_limit.to_f.round(3)}"
+    })
+  end
+
+end
